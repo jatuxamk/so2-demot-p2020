c004baec40f82ceb48290259584cfcbef58559f7
1909654-xamk <63038316+1909654-xamk@users.noreply.github.com>
2020-08-31 12:31:24 +0300

    Annin ehdotus

--- demo01/main.js
+++ demo01/main.js
@@ -1,25 +1,25 @@
-const http = require("http");
-const url = require("url");
+const http = require("http"); //vakion nimeäminen, http ominaisuudet käyttöön ohjelmaan 
+const url = require("url"); //osoiterivin tiedot
 
-const portti = 3001;
+const portti = 3001; //portin määritys
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => { //serverin luonti, req = request,  res = respond
 
     if (req.url != "/favicon.ico") {
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query; // poimitaan tiedot osoiteriviltä
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon"; //jost tiedot.nimi ei ole määritelty niin tulee "tuntematon"
     
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" }); //head osa, html
         res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
         res.write(`<h2>Heippa, ${nimi}!</h2>`);
-        res.end();
+        res.end(); //lopetus 
 
     }
 
 });
 
-serveri.listen(portti, () => {
-    console.log(`Palvelin käynnistyi porttiin ${portti}`);   
+serveri.listen(portti, () => { //serverin käynnistys
+    console.log(`Palvelin käynnistyi porttiin ${portti}`);   //teksti avautuu komentoriville kun serveri käynnistetään, portin numeroksi tulee aiemmin määritelty 3001
 });
