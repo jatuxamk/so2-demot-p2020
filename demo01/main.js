948429f2710dfcf3d450b639d35b82dbd1ca5cba
bheri004xamk <63038243+bheri004xamk@users.noreply.github.com>
2020-08-31 12:27:23 +0300

    Henri Riissanen testaa
    Tässä ehdotukset. Tehtiin porukassa Niko Laurila, Mikko Sistonen, Terhi Lahtinen, Pauli Hietanen

--- demo01/main.js
+++ demo01/main.js
@@ -1,25 +1,25 @@
-const http = require("http");
-const url = require("url");
+const http = require("http"); //luodaan olio, joka käyttää http-protokollaa
+const url = require("url"); //muuttuja, joka lukee selaimen urlin
 
-const portti = 3001;
+const portti = 3001; //asetetaan portin arvo
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => { //luodan palvelin 'serveri', joka käyttää http-protokollaa (http olio)
 
-    if (req.url != "/favicon.ico") {
+    if (req.url != "/favicon.ico") { //testaa, hakeeko pyyntö faviconia
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query; //luodaan JSON url-kentästä
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon"; //asetetaan muuttujan arvoksi url-JSONin nimi-tieto
     
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
-        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
-        res.write(`<h2>Heippa, ${nimi}!</h2>`);
-        res.end();
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" }); //kerrotaan selaimelle: kaikki ok, käytetään html:ää, merkkikoodaus sisältää ääkköset
+        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`); //kirjoitetaan sivulle otsikko
+        res.write(`<h2>Heippa, ${nimi}!</h2>`); //otetaan urlista tehty muuttuja, joka tulostetaan tervehdyksen mukana
+        res.end(); //loppu
 
     }
 
 });
 
-serveri.listen(portti, () => {
-    console.log(`Palvelin käynnistyi porttiin ${portti}`);   
+serveri.listen(portti, () => { //palvelin kuuntelee määritettyä porttia
+    console.log(`Palvelin käynnistyi porttiin ${portti}`);   //tulostetaan konsoliin ilmoitus palvelimen käynnistymisestä
 });
