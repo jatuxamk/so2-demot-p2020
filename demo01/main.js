12300bc59e767b1863c001844534f11d3a7c4489
Saara Häkkinen <63038331+asaha018xamk@users.noreply.github.com>
2020-08-31 12:20:51 +0300

    Saara Häkkinen
    harjoitus

--- demo01/main.js
+++ demo01/main.js
@@ -1,16 +1,17 @@
 const http = require("http");
 const url = require("url");
 
-const portti = 3001;
+const portti = 3001; //portin numero asetetaan vakioksi
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => { //serverin luominen
 
-    if (req.url != "/favicon.ico") {
+    if (req.url != "/favicon.ico") { //luettaessa tietoja urlista jättää faviconin huomiotta
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query; //hakee tiedot urlista
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon"; //jos urlissa ei nimi tietoa asetetaan arvoksi "tuntematon"
     
+        //html muotoilu
         res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
         res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
         res.write(`<h2>Heippa, ${nimi}!</h2>`);
@@ -21,5 +22,5 @@ const serveri = http.createServer((req, res) => {
 });
 
 serveri.listen(portti, () => {
-    console.log(`Palvelin käynnistyi porttiin ${portti}`);   
+    console.log(`Palvelin käynnistyi porttiin ${portti}`);   //tulostus konsoliin
 });
