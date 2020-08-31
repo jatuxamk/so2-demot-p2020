7eaa6556dfedc4c76d17d451e98965b0896a5dae
Henri Jauhiainen <aheja003@edu.xamk.fi>
2020-08-31 12:13:34 +0300

    Testi (Henri Jauhiainen)

--- demo01/main.js
+++ demo01/main.js
@@ -1,25 +1,25 @@
-const http = require("http");
-const url = require("url");
+const http = require("http"); //http-moduuli
+const url = require("url"); //url-moduuli
 
-const portti = 3001;
+const portti = 3001; //määritellään portti
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => { //tehdään palvelin
 
     if (req.url != "/favicon.ico") {
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query; // ottaa url-stringin ja palauttaa objektin
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon"; //nimi on tuntematon
     
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
-        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
-        res.write(`<h2>Heippa, ${nimi}!</h2>`);
-        res.end();
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" }); //statuskoodi, millaista sisältöä sivulla
+        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`); //hello world teksti
+        res.write(`<h2>Heippa, ${nimi}!</h2>`); //ottaa nimi-tiedot ylhäältä
+        res.end(); //loppuu
 
     }
 
 });
 
-serveri.listen(portti, () => {
+serveri.listen(portti, () => { //käynnistää palvelimen yllä määriteltyyn porttiin 
     console.log(`Palvelin käynnistyi porttiin ${portti}`);   
 });
