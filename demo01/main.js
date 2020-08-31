2463d7a97010bbe72372aedde34dff590ec2bdbf
btepu002xamk <63038413+btepu002xamk@users.noreply.github.com>
2020-08-31 12:51:48 +0300

    Update main.js

--- demo01/main.js
+++ demo01/main.js
@@ -1,25 +1,25 @@
 const http = require("http");
 const url = require("url");
 
-const portti = 3001;
+const portti = 3001; //Annetaan portinnumero
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => {  //Tehdään serveri 
 
-    if (req.url != "/favicon.ico") {
+    if (req.url != "/favicon.ico") { 
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query;  //Katsotaan nimi urlista
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";  //tulostetaan nimi, jos ei ole niin tuntetamaton
     
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
-        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
-        res.write(`<h2>Heippa, ${nimi}!</h2>`);
-        res.end();
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });  //perus koodialutukset
+        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`); //Html koodia mikä tulostuu aina 
+        res.write(`<h2>Heippa, ${nimi}!</h2>`); //tulostetaan heippa + annettu nimi
+        res.end(); //lopetus
 
     }
 
 });
 
-serveri.listen(portti, () => {
-    console.log(`Palvelin käynnistyi porttiin ${portti}`);   
+serveri.listen(portti, () => {   //katsotaan portti mihin käynnistytään
+    console.log(`Palvelin käynnistyi porttiin ${portti}`);     //consoleen ilmoitus että käynnistyi 
 });
