1e17ea7499e6e7831867dbd5254c328f978f19a2
Btela005Xamk <63038417+Btela005Xamk@users.noreply.github.com>
2020-08-31 12:29:21 +0300

    Terhi Lahtinen
    Mukana: Niko, Henri R, Ville, Oskari, Markus M, Mikko S, Aarni, Pauli

--- demo01/main.js
+++ demo01/main.js
@@ -1,25 +1,25 @@
-const http = require("http");
-const url = require("url");
+const http = require("http");   //Tehdään olio, joka käyttää http protokollaa
+const url = require("url");     //Muuttuja, joka lukee selaimen urlin
 
-const portti = 3001;
+const portti = 3001;    //luodaan portti
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => {   //Luodaan palvelin nimeltä serveri 
 
-    if (req.url != "/favicon.ico") {
+    if (req.url != "/favicon.ico") {    //Tarkistaa käytetäänkö faviconia
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query;    //Tehdään urlista json
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";  //Mikäli nimi tyhjä, tulostuu tuntematon(perusarvo)
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
-    
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
-        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
-        res.write(`<h2>Heippa, ${nimi}!</h2>`);
-        res.end();
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" }); //Selaimelle kerrotaan, että kaikki ok sekä html ja ääkköset
+        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`); //Perus tekstiä
+        res.write(`<h2>Heippa, ${nimi}!</h2>`);     //Teksti ja otetaan muuttuja urlkentästä
+        res.end(); //Loppu
 
     }
 
 });
 
+//Palvelin kuuntelee ennalta määrättyä porttia
 serveri.listen(portti, () => {
     console.log(`Palvelin käynnistyi porttiin ${portti}`);   
 });
