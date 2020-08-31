43768500818dd9f162f320b5892667e8402b2f7f
Mimmi Tick <63038379+bmiti002-xamk@users.noreply.github.com>
2020-08-31 12:43:46 +0300

    Update main.js
    Mimmi :(

--- demo01/main.js
+++ demo01/main.js
@@ -1,9 +1,9 @@
-const http = require("http");
+const http = require("http"); 
 const url = require("url");
 
-const portti = 3001;
+const portti = 3001; //määritellään portti mitä kuunnellaan
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => { //luodaan serveri jolla parametrit pyyntö ja vastaus
 
     if (req.url != "/favicon.ico") {
 
@@ -11,9 +11,9 @@ const serveri = http.createServer((req, res) => {
     
         let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
     
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
-        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
-        res.write(`<h2>Heippa, ${nimi}!</h2>`);
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" }); //html merkistömäärityksiä
+        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`); //vastauksena h1 tason otsikko
+        res.write(`<h2>Heippa, ${nimi}!</h2>`); //vastauksena h2 tason otsikko ja data muuttujasta nimi
         res.end();
 
     }
@@ -23,3 +23,5 @@ const serveri = http.createServer((req, res) => {
 serveri.listen(portti, () => {
     console.log(`Palvelin käynnistyi porttiin ${portti}`);   
 });
+
+//helvetti kun järki lähtee :(
