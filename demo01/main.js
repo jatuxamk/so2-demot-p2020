58484ab3027820024d28458acfd38b54e8c07ff4
NikoLaurila <63038296+NikoLaurila@users.noreply.github.com>
2020-08-31 12:25:01 +0300

    Update main.js
    tekemisen meiniki!
    
    Mukana Henri, Terhi, Pauli, Markus Manninen, Mikko ja tietysti minä!

--- demo01/main.js
+++ demo01/main.js
@@ -1,25 +1,25 @@
-const http = require("http");
-const url = require("url");
+const http = require("http"); // tehdään olio joka käyttää http protokollaa
+const url = require("url"); // tämä lukee selaimen url palkin?
 
-const portti = 3001;
+const portti = 3001; // käytetään porttia 3001
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => { // tässä luodaan palvelin nimeltään serveri 
 
-    if (req.url != "/favicon.ico") {
+    if (req.url != "/favicon.ico") { // tarkistetaan käytetäänkö fav iconia 
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query; // tehdään json urlista
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon"; // tiedolle nimi asetetaan perusarvo tuntematon
     
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
-        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
-        res.write(`<h2>Heippa, ${nimi}!</h2>`);
-        res.end();
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" }); // selaimelle kerrrotaan kaikki ok ja lisätään ääkköset 
+        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`); // kirjoitetaan sivulle otsikko
+        res.write(`<h2>Heippa, ${nimi}!</h2>`); // otetaan url kentästä ja tulostetaan
+        res.end(); //loppu
 
     }
 
 });
 
-serveri.listen(portti, () => {
-    console.log(`Palvelin käynnistyi porttiin ${portti}`);   
+serveri.listen(portti, () => { // palvelin kuuntelee ennalta määrättyä porttia
+    console.log(`Palvelin käynnistyi porttiin ${portti}`);   // console log tulostaa sen mitä tapahtuu
 });
