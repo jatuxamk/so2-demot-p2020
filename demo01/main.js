655aca27f533339c4def4034da41cac66440bd3f
henriN6 <70511823+henriN6@users.noreply.github.com>
2020-08-31 12:13:23 +0300

    Henri Niemi
    Kommentoi koodin osat ominsanoin/ymmärtäen

--- demo01/main.js
+++ demo01/main.js
@@ -1,25 +1,25 @@
-const http = require("http");
-const url = require("url");
+const http = require("http"); // Luodaan HTTP (vaaditaan)
+const url = require("url"); //Pyydetään URL (eli vaaditaan selaimeen url osoite)
 
-const portti = 3001;
+const portti = 3001; //portti mistä haetaan localhostista selaimesta
 
-const serveri = http.createServer((req, res) => {
+const serveri = http.createServer((req, res) => { //serverin olemassa olo pyyntö ja vastaus (request ja Response)
 
-    if (req.url != "/favicon.ico") {
+    if (req.url != "/favicon.ico") { //kysyy urlista kaiken mitä alhaalla on annettu komentoja.
 
-        let tiedot = url.parse(req.url, true).query;
+        let tiedot = url.parse(req.url, true).query; //kysyy urlista tiedot
     
-        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon";
+        let nimi = (tiedot.nimi) ? tiedot.nimi : "tuntematon"; //defaulttina let nimi tulostaa tuntematon mutta jos kirjoitat urliin nimen vaikka Risto se tulostaa tuntemattoman sijasta Riston. (ja määrittää sen)
     
-        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" });
-        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`);
-        res.write(`<h2>Heippa, ${nimi}!</h2>`);
-        res.end();
+        res.writeHead(200, { "Content-type" : "text/html; charset=utf-8" }); //head jolla saadaan enemmän "laadukasta" contenttia serverille
+        res.write(`<h1>Demo 1: Node.js -palvelinohjelmoinnin perusteita</h1>`); //kirjoittaa otsikon
+        res.write(`<h2>Heippa, ${nimi}!</h2>`); //kirjoittaa Heippa plus nimi H2 tyyliin.
+        res.end(); //lopettaa responsen
 
     }
 
 });
 
-serveri.listen(portti, () => {
-    console.log(`Palvelin käynnistyi porttiin ${portti}`);   
+serveri.listen(portti, () => { //serveri "kuuntelee" pyyntöä
+    console.log(`Palvelin käynnistyi porttiin ${portti}`);   //tulostaa consoleen jos se toimi oikein.
 });
