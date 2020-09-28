e5734b0937567dc2fc616ddc063c73dc4d487f8b
Janne Turunen <janne.turunen@xamk.fi>
2020-09-28 12:57:37 +0300

    Express ja EJS-templatet

--- demo04/main.js
+++ demo04/main.js
@@ -1,19 +1,38 @@
 const express = require("express");
-
 const app = express();
 
 const portti = 3004;
+
+const fs = require("fs");
 
 app.use(express.static("./public"));
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+let kayttajat = [];
+
+fs.readFile("./models/kayttajat.json", "utf-8", (err, data) => {
+
+    if (err) throw err;
+
+    kayttajat = JSON.parse(data);
+
+});
+
+app.get("/kayttaja/:id", (req, res) => {
+
+    let indeksi = req.params.id - 1;
+
+    let kayttaja = kayttajat[indeksi];
+
+    res.render("tiedot", { "kayttaja" : kayttaja });
+
+});
+
 app.get("/", (req, res) => {
 
-    let terveiset = "Heippa!";
-
-    res.render("index", { "terveiset" : terveiset });
+    res.render("index", { "kayttajat" : kayttajat });
 
 });
 
