3461899ae1c3bddb15937df57814577391a828ee
Janne Turunen <janne.turunen@xamk.fi>
2020-09-28 11:40:25 +0300

    Express ja EJS-template

--- demo04/main.js
+++ demo04/main.js
@@ -6,6 +6,17 @@ const portti = 3004;
 
 app.use(express.static("./public"));
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+
+    let terveiset = "Heippa!";
+
+    res.render("index", { "terveiset" : terveiset });
+
+});
+
 app.listen(portti, () => {
 
     console.log(`Palvelin käynnistyi porttiin: ${portti}`);
