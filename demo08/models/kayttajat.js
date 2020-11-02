c3dd5886c4bacf918c351bda8d3b8fc4dab92794
Janne Turunen <janne.turunen@xamk.fi>
2020-11-02 13:23:33 +0200

    Node.js ja MySQL (2)

--- demo08/models/kayttajat.js
+++ demo08/models/kayttajat.js
@@ -21,9 +21,56 @@ module.exports = {
 
     hae : (lomaketiedot, callback) => {
 
+        let ehdot = [];
+
+
+        if (lomaketiedot.luottokorttityyppi) {
+
+            let luottokorttiehdot;
+
+            if (Array.isArray(lomaketiedot.luottokorttityyppi)) {
+
+                let tyypit = lomaketiedot.luottokorttityyppi.map((tyyppi) => {
+
+                    return mysql.escape(tyyppi);
+
+                });
+
+                luottokorttiehdot = tyypit.join(" OR luottokorttityyppi = ");
+
+            } else {
+
+                luottokorttiehdot = mysql.escape(lomaketiedot.luottokorttityyppi);
+
+            }
+
+            ehdot.push(`(luottokorttityyppi = ${luottokorttiehdot})`);
+
+        }
+
+
+        if (lomaketiedot.sukupuoli == "Mies") {
+
+            ehdot.push(`(sukupuoli = \'Mies\')`);
+
+        }
+
+        if (lomaketiedot.sukupuoli == "Nainen") {
+
+            ehdot.push(`(sukupuoli = \'Nainen\')`);
+
+        }
+
+      
         let hakusana = mysql.escape(`%${lomaketiedot.hakusana}%`);
 
-        let sql = `SELECT * FROM kayttajat WHERE sukunimi LIKE ${hakusana} OR etunimi LIKE ${hakusana} LIMIT 50`;
+        let hakusanaehdot = `(sukunimi LIKE ${hakusana} OR etunimi LIKE ${hakusana})`;
+
+        ehdot.push(hakusanaehdot);
+
+        let sql = `SELECT * FROM kayttajat WHERE ${ehdot.join(" AND ")} LIMIT 50`;
+
+        console.log(sql); 
 
         yhteys.query(sql, (err, data) => {
 
